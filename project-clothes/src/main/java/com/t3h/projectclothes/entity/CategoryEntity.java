0ea5176fa0d186d3e02f490cc04fca41f03ac89e
TruonggDX <dinhxuantruong2003coi@gmail.com>
2025-12-15 15:15:47 +0700

    role

--- project-clothes/src/main/java/com/t3h/projectclothes/entity/CategoryEntity.java
+++ project-clothes/src/main/java/com/t3h/projectclothes/entity/CategoryEntity.java
@@ -1,11 +1,18 @@
 package com.t3h.projectclothes.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
-
-import static lombok.AccessLevel.PRIVATE;
 
 @Entity
 @Table(name = "categories")
