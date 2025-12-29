46b8c47603f6c4fbacbb08aabc8435f3982c0a74
sharkDuong <phantohiroxi@gmail.com>
2025-12-29 17:06:15 +0700

    JWT

--- project-clothes/src/main/java/com/t3h/projectclothes/repository/AccountRepository.java
+++ project-clothes/src/main/java/com/t3h/projectclothes/repository/AccountRepository.java
@@ -1,5 +1,12 @@
 package com.t3h.projectclothes.repository;
 
-public interface AccountRepository {
+import com.t3h.projectclothes.entity.UserEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface AccountRepository extends JpaRepository<UserEntity, Long> {
+
+  Optional<UserEntity> findByEmailAndIsDeletedFalse(String email);
 }
--- project-clothes/src/main/java/com/t3h/projectclothes/service/AuthService.java
+++ project-clothes/src/main/java/com/t3h/projectclothes/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.t3h.projectclothes.service;
 
+import com.t3h.projectclothes.dto.auth.LoginRequest;
+import com.t3h.projectclothes.dto.auth.LoginResponse;
+
 public interface AuthService {
 
+  LoginResponse login(LoginRequest request);
 }
